86766f341ca2894556689b647b6569f3c9eb5cd4
abdelrahmanyr <71361116+abdelrahmanyr@users.noreply.github.com>
2020-11-06 20:18:46 +0200

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -43,7 +43,7 @@ class MusicController(wavelink.Player):
 
             self.next.clear()
 
-            song = await self.queue[0]
+            song = self.queue[0]
             await player.play(song)
             self.now_playing = await self.channel.send(f":play_pause: | __Now playing:__ **{song}** **`[{(datetime.timedelta(seconds = int(song.length / 1000)))}]`**.")
             await self.next.wait()
