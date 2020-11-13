6af55ea8f5cb9a2d34dc901968d95043d2f75418
abdelrahmanyr <71361116+abdelrahmanyr@users.noreply.github.com>
2020-11-13 02:00:55 +0200

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -159,15 +159,15 @@ class Music(commands.Cog):
                         controller = self.get_controller(ctx)
                         await controller.queue.put(track)
 
-                        embed = discord.Embed(title = "Enqueued:",
-                                        description = f":play_pause: | **{str(track.title)}**",
+                        embed = discord.Embed(title = "Enqueued Stream:",
+                                        description = f":play_pause: | **{str(track)}**",
                                         color = discord.Colour.dark_red()
                                         )
                         embed.add_field(name = "Stream Player", value = f"**{ctx.message.author.mention}**")
 
                     if not player.is_playing:
-                        embed = discord.Embed(title = "Playing:",
-                                        description = f"**:play_pause: | {str(track.title)}**",
+                        embed = discord.Embed(title = "Playing Stream:",
+                                        description = f"**:play_pause: | {str(track)}**",
                                         color = discord.Colour.dark_red()
                                         )
                         embed.add_field(name = "Track Player", value = f"**{ctx.message.author.mention}**")
