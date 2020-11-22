3a3a800637b03e4b4b5a7e201553bf356e7c8a51
abdelrahmanyr <71361116+abdelrahmanyr@users.noreply.github.com>
2020-11-23 01:22:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -82,8 +82,9 @@ async def shorten(ctx, url : str = None):
                           colour = discord.Colour.dark_red()
                          )
     embed.set_author(name = "MayBot 🎸", icon_url = client.user.avatar_url)
-    embed.add_field(name = "Why do I have to pass through an advertisement page ?", value = "The bot services is completely free and it's the only way to support the developer \n__Note:__ if you didn't shorten a link you will be redirected to some cool song, have fun!")
+    embed.add_field(name = "Why do I have to pass through an advertisement page ?", value = "The bot services are completely free and it's the only way to support the developer \n__Note:__ if you didn't shorten a link you will be redirected to some cool song, have fun!")
     embed.add_field(name = "What do I benefit from using this command ?", value = "Having a short url instead of a long messy one.")
+    embed.add_field(name = "Wanna try the same things and earn money ?", value = f"__[Click the link and sign in.](http://join-shortest.com/ref/15860a2afd?user-type=new)__")
     embed.set_footer(text = f"Shortened by: {ctx.message.author}", icon_url = ctx.message.author.avatar_url)
     await ctx.send(embed = embed)
 
