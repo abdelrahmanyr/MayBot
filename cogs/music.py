7bab47096a442a0cd03dd3b554d86858a4ce9130
abdelrahmanyr <71361116+abdelrahmanyr@users.noreply.github.com>
2021-03-08 22:59:24 +0200

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -209,8 +209,7 @@ class Music(commands.Cog):
                 if query.startswith("https://open.spotify.com/track"):
                     name, url, artist, cover = self.spotify_track(query)
                     tracks = await self.bot.wavelink.get_tracks(f"ytsearch:{name} - {artist}")
-                    print(tracks)
-                    tracks[0].name, tracks[0].uri, tracks[0].thumb = f"{name} - {artist}", url, cover
+                    tracks[0].title, tracks[0].uri, tracks[0].thumb = f"{name} - {artist}", url, cover
                     track = Track(tracks[0].id, tracks[0].info, requester = ctx.author)
                     controller = self.get_controller(ctx)
                     await controller.queue.put(track)
