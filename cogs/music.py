02866c1a2d5c989d7a2c319cad194c0d23d6fbf4
abdelrahmanyr <71361116+abdelrahmanyr@users.noreply.github.com>
2021-03-09 19:00:16 +0200

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -142,14 +142,14 @@ class Music(commands.Cog):
                             color = discord.Colour.dark_red()
                             )
             embed.add_field(name = "Track Player", value = f"**{ctx.message.author.mention}**")
-            embed.add_field(name = "Track Duration", value = f"**`[{self.format_time(track.length)}]`**", inline = True)
+            embed.add_field(name = "Track Duration", value = f"**`[{self.format_time(track.length)}]`**", inline = False)
         if not player.is_playing:
             embed = discord.Embed(title = "Playing:",
                             description = f":play_pause: | __**[{str(track)}]({track.uri})**__",
                             color = discord.Colour.dark_red()
                             )
             embed.add_field(name = "Track Player", value = f"**{ctx.message.author.mention}**")
-            embed.add_field(name = "Track Duration", value = f"**`[{self.format_time(track.length)}]`**", inline = True)
+            embed.add_field(name = "Track Duration", value = f"**`[{self.format_time(track.length)}]`**", inline = False)
         return embed
     
     def spotify_track(self, link):
