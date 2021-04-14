54b81923eabbce330a8b8fbe1e6edc6847acde64
abdelrahmanyr <71361116+abdelrahmanyr@users.noreply.github.com>
2021-04-14 02:32:04 +0200

    Update roleplay commands' GIFs

--- cogs/roleplay.py
+++ cogs/roleplay.py
@@ -14,9 +14,11 @@ class Roleplay(commands.Cog):
                                   color = discord.Colour.dark_red()
                                  )
             embed.set_image(url = random.choice(gifs))
-        elif member in [str(ctx.author.mention), "me", "myself"]:
+        elif member in [ctx.author.mention, "me", "myself"]:
 
-            embed = discord.Embed(description = f"{emoji} **{ctx.message.author.name}** {verb_2} himself")
+            embed = discord.Embed(description = f"{emoji} **{ctx.message.author.name}** {verb_2} himself",
+                                  color = discord.Colour.dark_red()
+                                 )
             embed.set_image(url = random.choice(gifs))
         else:
             embed = discord.Embed(description = f"{emoji} | **{ctx.message.author.name}** {verb} {member}",
@@ -31,6 +33,12 @@ class Roleplay(commands.Cog):
                 "https://media.giphy.com/media/PajqfRMX30hBtgKqF0/giphy.gif",       #2
                 "https://media.giphy.com/media/z9RDFpfeXeBAQoz7Ia/giphy.gif",       #3
                 "https://media.giphy.com/media/VJ3ItCxd2zwJlIRzQ5/giphy.gif",       #4
+                "https://media.giphy.com/media/QJiQc6XQMPUKDEXPFr/giphy.gif",       #5
+                "https://media.giphy.com/media/8cWn0HSLA9y1tUgIk6/giphy.gif",       #6
+                "https://media.giphy.com/media/wwfL6DcylpXnpSX3kK/giphy.gif",       #7
+                "https://media.giphy.com/media/OjXngB124TpXCidFkN/giphy.gif",       #8
+                "https://media.giphy.com/media/xZ6lafczLTaz4WxaTG/giphy.gif",       #9
+                "https://media.giphy.com/media/4dVYiJ5mscFKjMBLua/giphy.gif"        #10
                ]
 
         embed = self.act(ctx, member, ":crossed_swords:", "fights", "fights", gifs)
@@ -39,6 +47,25 @@ class Roleplay(commands.Cog):
     @commands.command()
     async def hug(self, ctx, *, member = None):
         gifs = ["https://media.giphy.com/media/1dhiqEvbGLKLqAa7hx/giphy.gif",       #1
+                "https://media.giphy.com/media/AUJk6rDzwZc2olNAYX/giphy.gif",       #2
+                "https://media.giphy.com/media/6RoLRaTqIzPqTXDFAf/giphy.gif",       #3
+                "https://media.giphy.com/media/iHI2xEBBMmW375vlNj/giphy.gif",       #4
+                "https://media.giphy.com/media/MFJdkbanrBljXlMRQy/giphy.gif",       #5
+                "https://media.giphy.com/media/MuxRlhmbK3BxEE3MCz/giphy.gif",       #6
+                "https://media.giphy.com/media/VR3XYlyEaSgF6dua3z/giphy.gif",       #7
+                "https://media.giphy.com/media/22ikiUfzxhYy2CC0Xi/giphy.gif",       #8
+                "https://media.giphy.com/media/5y0Hix0NVHzqKS0lj3/giphy.gif",       #9
+                "https://media.giphy.com/media/63pQ3XzytCOtLBNfjv/giphy.gif",       #10
+                "https://media.giphy.com/media/3X9ObZeRWOaltm0aaU/giphy.gif",       #11
+                "https://media.giphy.com/media/2wQ7alRjhG3kY9uL5q/giphy.gif",       #12
+                "https://media.giphy.com/media/IXkff06c1LvuXrG95w/giphy.gif",       #13
+                "https://media.giphy.com/media/X7wZ41n66PWtW5hImr/giphy.gif",       #14
+                "https://media.giphy.com/media/IVJVWQYyj16CNdCWmn/giphy.gif",       #15
+                "https://media.giphy.com/media/98qlxqjXqBE8rQh9EQ/giphy.gif",       #16
+                "https://media.giphy.com/media/MyWEnfYsfdq02N7PxR/giphy.gif",       #17
+                "https://media.giphy.com/media/tiEQ1F8MrFgNnJI8bV/giphy.gif",       #18
+                "https://media.giphy.com/media/1YrVRoPhSfbKsIHDaQ/giphy.gif",       #19
+                "https://media.giphy.com/media/Awku1OLn1ZsVazioTe/giphy.gif"        #20
                ]
         embed = self.act(ctx, member, ":people_hugging:", "hugs", "hugs", gifs)
         await ctx.send(embed = embed)
@@ -46,7 +73,15 @@ class Roleplay(commands.Cog):
     @commands.command()
     async def sleep(self, ctx, *, member = None):
         gifs = ["https://media.giphy.com/media/SpajToJTAyuFU0GXDN/giphy.gif",       #1
-                "https://media.giphy.com/media/hk8Epk9TVvsCyj8XVT/giphy.gif"        #2
+                "https://media.giphy.com/media/hk8Epk9TVvsCyj8XVT/giphy.gif",       #2
+                "https://media.giphy.com/media/8b5mRdK8dalVFyLCvR/giphy.gif",       #3
+                "https://media.giphy.com/media/h7nIxcZlZsQhXGVm9m/giphy.gif",       #4
+                "https://media.giphy.com/media/p694ppEoOztCCRyBb5/giphy.gif",       #5
+                "https://media.giphy.com/media/atsX70LlSZyUnd2xHh/giphy.gif",       #6
+                "https://media.giphy.com/media/KigYEljFltNtZtMjH7/giphy.gif",       #7
+                "https://media.giphy.com/media/zgjAftzZbjVgoYASR7/giphy.gif",       #8
+                "https://media.giphy.com/media/tm1pbMpAxCOOEpy6iC/giphy.gif",       #9
+                "https://media.giphy.com/media/tm1pbMpAxCOOEpy6iC/giphy.gif",       #10
                ]
         embed = self.act(ctx, member, ":sleeping_accommodation:", "sleeps", "sleeps with", gifs)
         await ctx.send(embed = embed)
@@ -54,7 +89,15 @@ class Roleplay(commands.Cog):
     @commands.command()
     async def slap(self, ctx, *, member = None):
         gifs = ["https://media.giphy.com/media/iF50YPp3fwEl1QohZc/giphy.gif",       #1
-                "https://media.giphy.com/media/OBsLisJ3I0H7W4cnGY/giphy.gif"        #2
+                "https://media.giphy.com/media/OBsLisJ3I0H7W4cnGY/giphy.gif",       #2
+                "https://media.giphy.com/media/Uou642KMOEEllknUdZ/giphy.gif",       #3
+                "https://media.giphy.com/media/uIjQpTstEKruHOTc8h/giphy.gif",       #4
+                "https://media.giphy.com/media/bwcZJTdPSlA2WLUbdT/giphy.gif",       #5
+                "https://media.giphy.com/media/vThtVgF8CM675e8dUw/giphy.gif",       #6
+                "https://media.giphy.com/media/Wcn03mErixaly1v3Rm/giphy.gif",       #7
+                "https://media.giphy.com/media/dD08Mbh3dD8ofIJF1Q/giphy.gif",       #8
+                "https://media.giphy.com/media/RSEiZIyJ6RMkXYqZfs/giphy.gif",       #9
+                "https://media.giphy.com/media/QIxhNYBNWtpHInCQtQ/giphy.gif"        #10
                ]
 
         embed = self.act(ctx, member, ":punch:", "slaps", "slaps", gifs)
@@ -97,12 +140,28 @@ class Roleplay(commands.Cog):
         await ctx.send(embed = embed)
 
     @commands.command()
+    async def pat(self, ctx, *, member = None):
+        gifs = ["https://media.giphy.com/media/oaHbYKKZKXzPlP0tVv/giphy.gif",       #1
+                "https://media.giphy.com/media/i4la05Vy96Dt7Kbeks/giphy.gif",       #2
+                "https://media.giphy.com/media/rHqM4aGWrjkWBBeeBG/giphy.gif",       #3
+                "https://media.giphy.com/media/zaG1uyrhisset0J6Z5/giphy.gif",       #4
+                "https://media.giphy.com/media/aaEGxd50vlsezlVLVv/giphy.gif",       #5
+                "https://media.giphy.com/media/0riNaUmpAYzPWbQusl/giphy.gif",       #6
+                "https://media.giphy.com/media/50EuqJ0rrmfxWkCUQk/giphy.gif",       #7
+                "https://media.giphy.com/media/RcY34P516NDQp0jgMN/giphy.gif",       #8
+                "https://media.giphy.com/media/XFwVUl21YiedB3lBMj/giphy.gif",       #9
+                "https://media.giphy.com/media/7RIAM3GJEFegbhU32B/giphy.gif"        #10
+               ]
+        embed = self.act(ctx, member, ":pleading_face:", "pats", "pats", gifs)
+        await ctx.send(embed = embed)
+
+    @commands.command()
     async def cry(self, ctx, *, member = None):
         gifs = ["https://media.giphy.com/media/zSoTFOinvxmY0Q9XmC/giphy.gif",       #1
                 "https://media.giphy.com/media/PrZeRLzye6CX5yGN0d/giphy.gif",       #2
                 "https://media.giphy.com/media/xXPNnDc6Idt7cbhPJB/giphy.gif",       #3
                 "https://media.giphy.com/media/NnoONS016J70R6ZUED/giphy.gif",       #4
-                "hhttps://media.giphy.com/media/NWfbpjFIoTylVnn6wj/giphy.gif",      #5
+                "https://media.giphy.com/media/NWfbpjFIoTylVnn6wj/giphy.gif",       #5
                 "https://media.giphy.com/media/6W8Oxugf8NR4EqeWt1/giphy.gif"        #6
                ]
         embed = self.act(ctx, member, ":droplet:", "cries", "cries with", gifs)
