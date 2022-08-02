70d599ab75a3a23805baf9f584d0147e61c2fc8e
Abdelrahman <71361116+abdelrahmanyr@users.noreply.github.com>
2022-08-02 18:57:23 +0200

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -146,10 +146,10 @@ class Music(commands.Cog):
 
         # Initiate our nodes. For this example we will use one server.
         # Region should be a discord.py guild.region e.g sydney or us_central (Though this is not technically required)
-        node = await self.bot.wavelink.initiate_node(host = "lavalink-nitrixexe.ml",
-                                                     port = 19147,
-                                                     rest_uri = "lavalink-nitrixexe.ml:19147",
-                                                     password = "",
+        node = await self.bot.wavelink.initiate_node(host = "lavalink.oops.wtf",
+                                                     port = 2000,
+                                                     rest_uri = "lavalink.oops.wtf:2000",
+                                                     password = "www.freehosts.ga",
                                                      identifier = "TEST",
                                                      region = "europe")
         node.set_hook(self.on_event_hook)
